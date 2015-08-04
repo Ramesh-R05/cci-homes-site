e0758b85599963d9f372ac0ace8d79501f0513b7
Cody Jenkins <iknowcss@gmail.com>
2015-08-04 17:30:44 +1000

    Export faceted module handlers so that children can explicitly set them

--- app/stores/facetedStores/taggedArticles.js
+++ app/stores/facetedStores/taggedArticles.js
@@ -1,8 +1,10 @@
-import FacetedModuleStore from '../facetedStores/facetedModule';
+import FacetedModuleStore, {handlers} from '../facetedStores/facetedModule';
 
 export default class TaggedArticlesStore extends FacetedModuleStore {
 
     static storeName = 'TaggedArticlesStore';
+
+    static handlers = handlers;
 
     constructor(dispatcher) {
         super(dispatcher, 'taggedArticles');
