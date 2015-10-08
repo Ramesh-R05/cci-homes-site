7b374d552c951ceea473008fc58dfca13ffdf72a
JSrirattanapibun <JSrirattanapibun@bauer-media.com.au>
2015-10-08 15:05:41 +1100

    Adding the steps to verify footer on homepage, section landing and article pages.

--- automation/homes/features/global_footer.feature
+++ automation/homes/features/global_footer.feature
@@ -1,14 +1,32 @@
 @local @global-footer @javascript
 Feature: The global footer should appear at the bottom of the page
     
+    @DHO-35 @DHO-217 @DHO-360
     Scenario: Check the global footer is displayed correctly on Homepage
         Given I am on the homepage
         Then I should see the global footer
-        And I should see the global footer heading "GET SOCIAL"
-        And I should see the Privacy Policy, Advertise and Terms of Use links
-
+        And I should see the Facebook, Twitter and Instagram links open in a new window
+        And I should see the newsletter sign up
+        And I should see magshop logo and subscribe links to "https://www.magshop.com.au/store/homestolove" in a new window
+        And I should see Belle, Real Living, Homes+ and House & Garden links redirected to the brand page in the current window
+        And I should see the Privacy Policy, Advertise and Terms of Use links open in a new window
+    
+    @DHO-35 @DHO-217 @DHO-360
     Scenario: Check the global footer is displayed correctly on section landing page
         Given I am on the "section" landing page
         Then I should see the global footer
-        And I should see the global footer heading "GET SOCIAL"
-        And I should see the Privacy Policy, Advertise and Terms of Use links
+        And I should see the Facebook, Twitter and Instagram links open in a new window
+        And I should see the newsletter sign up
+        And I should see magshop logo and subscribe links to "https://www.magshop.com.au/store/homestolove" in a new window
+        And I should see Belle, Real Living, Homes+ and House & Garden links redirected to the brand page in the current window
+        And I should see the Privacy Policy, Advertise and Terms of Use links open in a new window
+
+    @DHO-218 @DHO-217 @DHO-360
+    Scenario: Check the global footer is displayed correctly on article page
+        Given I am viewing an article
+        Then I should see the global footer
+        And I should not see the Facebook, Twitter and Instagram links
+        And I should see the newsletter sign up
+        And I should not see magshop logo and subscribe links
+        And I should see Belle, Real Living, Homes+ and House & Garden links redirected to the brand page in the current window
+        And I should see the Privacy Policy, Advertise and Terms of Use links open in a new window
--- automation/homes/features/step_definitions/global_footer.rb
+++ automation/homes/features/step_definitions/global_footer.rb
@@ -2,14 +2,57 @@ Then(/^I should see the global footer$/) do
     expect(page).to have_selector('.footer')
 end
 
-Then(/^I should see the global footer heading "(.*?)"$/) do |heading|
-    expect(page.find('.footer > section.get-social')).to have_content(heading.upcase.gsub(' ', '').upcase)
-end
-
-Then(/^I should see the Privacy Policy, Advertise and Terms of Use links$/) do
+Then(/^I should see the Privacy Policy, Advertise and Terms of Use links open in a new window$/) do
 	within('.footer__navigation') do
-  	    expect(page).to have_link('Privacy Policy', :href=>'http://www.bauer-media.com.au/privacy')
+  	    expect(page).to have_link('Privacy Policy', :href => 'http://www.bauer-media.com.au/privacy')
   	    expect(page).to have_link('Advertise', :href=>'http://www.bauer-media.com.au/advertising/advertise-with-us')
   	    expect(page).to have_link('Terms of Use', :href=>'http://www.bauer-media.com.au/terms/website-terms')
+
+  	    expect(find_link('Privacy Policy')[:target]).to eq('_blank')
+  	    expect(find_link('Advertise')[:target]).to eq('_blank')
+  	    expect(find_link('Terms of Use')[:target]).to eq('_blank')
   	end
+end
+
+Then(/^I (should|should not) see the Facebook, Twitter and Instagram links(?: open in a new window)?$/) do |status|
+	if (status == "should")
+        expected_content = Array.new(['http://www.facebook.com/homestoloveau', 
+    	    'http://twitter.com/homestoloveau', 
+    	    'http://instagram.com/homestoloveau/'])
+
+        actual_content = all('.social-link a').map { |a| a['href'] }
+        expect(actual_content).to eq(expected_content)
+
+        actual_target = all('.social-link a').map { |a| a['target'] }
+        expect(actual_target.count("_blank")).to eq(actual_target.size)
+    else
+        expect(page).to have_no_selector('.get-social')
+    end
+end
+
+Then(/^I should see Belle, Real Living, Homes\+ and House & Garden links redirected to the brand page in the current window$/) do
+    expected_content = Array.new(["#{$base_url}/belle/",
+    	"#{$base_url}/real-living/",
+    	"#{$base_url}/homes-plus/",
+    	"#{$base_url}/australian-house-and-garden/"])    
+    
+    actual_content = all('.network-info__supported-content a').map { |a| a['href'] }
+    expect(actual_content).to eq(expected_content)
+
+    actual_target = all('.network-info__supported-content a').map { |a| a['target'] }
+    expect(actual_target.count("_blank")).to eq(0)
+end
+
+Then(/^I should see the newsletter sign up$/) do
+    expect(find('.newsletter').visible?).to eq(true)
+end 
+
+Then(/^I (should|should not) see magshop logo and subscribe links(?: to "([^"]+)" in a new window)?$/) do |status, url|
+    if (status == "should")
+        expect(find('.magshop img')[:src]).to_not be_empty
+    	expect(find('.magshop a', match: :first)).to have_xpath("//a[@href = '#{url}' and @target = '_blank']")
+    	expect(find('.button--subscribe')).to have_xpath("//a[@href = '#{url}' and @target = '_blank']")
+    else
+        expect(page).to have_no_selector('.magshop')
+    end
 end
