049bdb70a38cbd8d10fc803dfa4b180288f363fe
JSrirattanapibun <JSrirattanapibun@bauer-media.com.au>
2015-10-15 15:59:08 +1100

    Adding the step to verify WN header on both desktop and mobile views.

--- automation/homes/features/step_definitions/network_header_steps.rb
+++ automation/homes/features/step_definitions/network_header_steps.rb
@@ -1,29 +1,22 @@
 Then(/^I should see the "([^"]+)" menu item with href "([^"]+)"$/) do |menu_item, href|
-  expect(page.find('.global-nav-list')).to have_link(menu_item, :href=>href)
+    expect(page.find('.global-nav-list')).to have_link(menu_item, :href=>href)
 end
 
 When(/^I hover on the "More" menu item$/) do
-  page.find('.global-nav-list .tl-header__link span', text: 'More').hover
+	page.find('.global-nav-list__has-children .tl-header__link span', text: 'More')
+	page.find('.global-nav-list__has-children .tl-header__link span', text: 'More').hover
 end
 
-Then(/^I should see the "(.*?)" menu heading$/) do |menu_heading|
-  expect(page.find('.global-nav-list__dropdown').find('.global-nav-list__title')).to have_content(menu_heading.upcase)
+Then(/^I should see the "([^"]+)" menu heading$/) do |menu_heading|
+    expect(page.find('.global-nav-list__dropdown .global-nav-list__title')).to have_content(menu_heading.upcase)
 end
 
 When(/^I click on the network header logo$/) do
-  page.find('.tl-header__logo-menu').click
+    page.find('.tl-header__logo-menu').click
 end
 
 Then(/^I should see the expanded menu/) do
-  expect(page).to have_selector('.tl-modal--menu')
-end
-
-Then(/^I should see the "(.*?)" logo menu item with href "(.*?)"$/) do |title, href|
-  expect(page.find(:link, {:href => href})['title']).to eql(title)
-end
-
-Then(/^I should see the "(.*?)" text menu item with href "(.*?)"$/) do |menu_item, href|
-  expect(page.find('.tl-modal--menu')).to have_link(menu_item, :href=>href)
+    expect(page).to have_selector('.tl-modal--menu')
 end
 
 Then(/^I should see the following sites opened in the current window with the specific url:$/) do |table|
@@ -35,17 +28,29 @@ Then(/^I should see the following sites opened in the current window with the sp
     end
  end
 
-Then(/^I should see the additionals sites opened in the current window with the specific url:$/) do |table|
-	all_actual_content_name = all('.global-nav-list__dropdown li a').collect(&:text)
-	all_actual_content_url = all('.global-nav-list__dropdown li a').map { |a| a['href'] }
-
-	table.hashes.each_with_index do |hash,index|
-		all_actual_content_name.should include(hash['name'])
-        all_actual_content_url.should include(hash['url'])
+Then(/^I should see the additionals (sites|links) opened in the current window with the specific url:$/) do |type, table|
+    if (type == "links")
+    	element = "mobile-menu-column"
+    else
+    	element = "global-nav-list__dropdown"
     end
 
-    all_actual_content_target = all('.global-nav-list__dropdown li a').map { |a| a['target'] }
+	all_actual_content_name = page.all(".#{element} li a").collect(&:text).reject(&:empty?)
+	all_actual_content_url = page.all(".#{element} li a").map { |a| a['href'] }
+
+    all_actual_content_target = all(".#{element} li a").map { |a| a['target'] }
     expect(all_actual_content_target.count("_blank")).to eq(0)
 end
 
+Then(/^I should see the following logos opened in the current window with the specific url:$/) do |table|
+	all_actual_image = all('.mobile-menu-column--image img').map { |img| img['alt'] }
+	all_actual_url = all('.mobile-menu-column--image a').map { |a| a['href'] }
 
+	table.hashes.each_with_index do |hash,index|
+	    all_actual_image.should include(hash['name'])
+        all_actual_url.should include(hash['url'])
+    end
+
+    all_actual_target = all('.mobile-menu-column--image a').map { |a| a['target'] }
+    expect(all_actual_target.count("_blank")).to eq(0)
+end
