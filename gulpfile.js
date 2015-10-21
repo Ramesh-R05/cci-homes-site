78d8b95e9cc93d35eabe90a8452d3f8bed339350
hatzibauer <thatzi@bauer-media.com.au>
2015-10-21 17:03:33 +1100

    DHO-342 implement native header on section pages

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ require('babel/register')({
 var config = require('@bxm/node-tasks/lib/config');
 config.style.glob = 'styles/**/*.scss';
 config.style.path = './styles/';
-config.style.paths.push('./node_modules/@bxm/gallery/styles');
+config.style.paths.push('./node_modules/@bxm/gallery/styles', './node_modules/@bxm/ad/styles');
 config.style.main = 'main.scss';
 config.lint.js = true;
 config.lint.scss = true;
