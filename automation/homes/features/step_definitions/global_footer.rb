465335cbdcedfc7dca199ea7ed9f1b352477c046
alejandro <asanchezgiraldo@bauer-media.com.au>
2016-06-03 09:36:45 +1000

    Fix/automation logo move (#63)
    * fix for homes logo move
    
    * fix for homes logo move and added test around the hambureger menu
    
    * remove steps from footer

--- automation/homes/features/step_definitions/global_footer.rb
+++ automation/homes/features/step_definitions/global_footer.rb
@@ -20,8 +20,8 @@ Then(/^I should see the Privacy Policy, Advertise and Terms of Use links open in
 end
 
 Then(/^I should see the Facebook, Twitter and Instagram links(?: open in a new window)?$/) do
-    expected_content = Array.new(["#{$homes_facebook_url}", 
-    	"#{$homes_twitter_url}", 
+    expected_content = Array.new(["#{$homes_facebook_url}",
+    	"#{$homes_twitter_url}",
     	"#{$homes_instagram_url}"])
 
     actual_content = all('.social-link a').map { |a| a['href'] }
@@ -31,22 +31,9 @@ Then(/^I should see the Facebook, Twitter and Instagram links(?: open in a new w
     expect(actual_target.count("_blank")).to eq(actual_target.size)
 end
 
-Then(/^I should see Belle, Real Living, Homes\+ and House & Garden links redirected to the brand page in the current window$/) do
-    expected_content = Array.new(["#{$base_url}belle/",
-    	"#{$base_url}real-living/",
-    	"#{$base_url}homes-plus/",
-    	"#{$base_url}australian-house-and-garden/"])    
-    
-    actual_content = all('.network-info__supported-content a').map { |a| a['href'] }
-    expect(actual_content).to eq(expected_content)
-
-    actual_target = all('.network-info__supported-content a').map { |a| a['target'] }
-    expect(actual_target.count("_blank")).to eq(0)
-end
-
 Then(/^I should see the newsletter sign up$/) do
     expect(find('.newsletter').visible?).to eq(true)
-end 
+end
 
 Then(/^I should see subscribe image and button(?: links to "([^"]+)" in a new window)?$/) do |url|
     actual_content = all('.magshop a').map { |a| a['href'] }
@@ -62,4 +49,41 @@ Then(/^I should see the subscribe button(?: links to "([^"]+)" in a new window)?
 
     actual_target = all('.magshop a.button').map { |a| a['target'] }
     expect(actual_target.count("_blank")).to eq(1)
+end
+
+#-------------- the  below are for the header
+Then(/^I should see Belle, Real Living, Homes\+ and House & Garden links redirected to the brand page in the current window$/) do
+    expected_content = Array.new(["#{$base_url}belle/",
+                                  "#{$base_url}real-living/",
+                                  "#{$base_url}homes-plus/",
+                                  "#{$base_url}australian-house-and-garden/"])
+
+    actual_content = all('.header-sponsors.header-sponsors--text-only a').map { |a| a['href'] }
+    expect(actual_content).to eq(expected_content)
+
+    actual_target = all('.network-info__supported-content a').map { |a| a['target'] }
+    expect(actual_target.count("_blank")).to eq(0)
+end
+
+#for homepage
+Then(/^I should see Belle, Real Living, Homes\+ and House & Garden links redirected$/) do
+    expected_content = Array.new(["#{$base_url}belle/",
+                                  "#{$base_url}real-living/",
+                                  "#{$base_url}homes-plus/",
+                                  "#{$base_url}australian-house-and-garden/"])
+
+    actual_content = all('.header-sponsors a').map { |a| a['href'] }
+    expect(actual_content).to eq(expected_content)
+end
+
+#for hamburger
+Then(/^I should see Belle, Real Living, Homes\+ and House & Garden links in the hamburger/) do
+    sleep(3)
+    expected_content = Array.new(["#{$base_url}belle/",
+                                  "#{$base_url}real-living/",
+                                  "#{$base_url}homes-plus/",
+                                  "#{$base_url}australian-house-and-garden/"])
+
+    actual_content = all('.header-sponsors--text-vertical-only a').map { |a| a['href'] }
+    expect(actual_content).to eq(expected_content)
 end
