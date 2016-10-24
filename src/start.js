a4ab915e7f5a1a0b1233a644c084923befe728c4
Kevin Heard <kev@kevinheard.com>
2016-10-25 09:53:12 +1100

    added new relic integration (#99)

--- src/start.js
+++ src/start.js
@@ -1,3 +1,6 @@
+if (!process.env.APP_KEY) throw new Error('APP_KEY environment variable not set');
+
+require('@bxm/node-apm')(process.env.APP_KEY);
 require('babel-polyfill');
 require('babel-register');
 
