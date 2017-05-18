679a4b1e9369180672b40c12bb532ee266480828
Nick Prodromou <hello@nickpro.com.au>
2017-05-18 10:11:23 +1000

    fix/navigation-section-middleware (#216)

--- src/app/server/bff/middleware/navSection.js
+++ src/app/server/bff/middleware/navSection.js
@@ -14,17 +14,15 @@ export default async function navSection(req, res, next) {
 
         const entityResponse = await makeRequest(`${req.app.config.services.remote.entity}/section/${navSection}`);
 
-        const navigationTag = (entityResponse.tagsDetails || []).find((tag) => {
-            return tag.name.includes('Homes navigation')
-        });
+        const sectionTag = entityResponse.tagsDetails[0];
 
-        entityResponse.kingtag = (navigationTag && navigationTag.urlName) || '';
+        entityResponse.kingtag = (sectionTag && sectionTag.urlName) || '';
 
         const pageSize = 12;
         const pageNo = parseInt(req.query.pageNo || 1, 10);
         const skip =  (pageNo-1) * pageSize;
 
-        const filter = `tagsDetails/fullName eq '${navigationTag.fullName}'`;
+        const filter = `tagsDetails/fullName eq '${sectionTag.fullName}'`;
         const [latestTeasersResp, galleryListingResponse] = await Promise.all([
             getLatestTeasers(itemsCount, skip, filter),
             makeRequest(`${req.app.config.services.remote.listings}/teasers?$select=*&$filter=nodeTypeAlias eq 'Gallery' and ${filter}&$orderby=pageDateCreated desc&$top=5`)
