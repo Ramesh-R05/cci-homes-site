36fb4509d47f71b16de0b997876a81288efb9a6f
Abhibxm <asharma@bauerxcelmedia.com.au>
2017-07-18 16:35:50 +1000

    Added Headless Chrome in chimp (#277)
    * Added Headless Chrome in chimp
    
    * Removed unused phantom step in package

--- automation/chimp.js
+++ automation/chimp.js
@@ -11,7 +11,7 @@ module.exports = {
     webdriverio: {
         desiredCapabilities: {
             chromeOptions: {
-                args: ["--enable-automation"]
+                args: ["--enable-automation", "headless", "disable-gpu"]
             }
         }
     }
