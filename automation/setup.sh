6c66ac50d971dc6268102df8519c01b1b17a2638
Abhibxm <asharma@bauerxcelmedia.com.au>
2017-07-19 17:05:13 +1000

    Update setup.sh (#285)

--- automation/setup.sh
+++ automation/setup.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-curl https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
-source ~/.nvm/nvm.sh
-nvm install "6.1.0"
-nvm use "6.1.0"
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
