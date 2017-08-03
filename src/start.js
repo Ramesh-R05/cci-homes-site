cfb5124dc48853ad920469710009e4b1dba9ca44
Greg <sharpdressedcodes@users.noreply.github.com>
2017-08-03 10:47:05 +1000

    OG:IMAGE crop params. Update server to 22.3.1 (#297)
    * OG:IMAGE crop params. Update server to 22.3.1
    * regenerate yarn lock post rebase
    * upgrade site for server package with webpack

--- src/start.js
+++ src/start.js
@@ -1,31 +1,42 @@
 process.env.APP_KEY = 'homes-site';
 process.title = process.env.APP_KEY;
+process.on('uncaughtException', function(e) {
+    throw e;
+});
 require('babel-polyfill');
 require('babel-register');
 var logger = require('./logger');
 require('./apm');
 var fs = require('fs');
-var requiredFile = './dist/scripts/main.js';
+var requiredFile = './dist/manifest.json';
 var retryDelay = 5000;
 var attemptCount = 0;
 var maxAttempts = 12;
+var timerId = null;
 
 function startWhenReady() {
     attemptCount++;
-    clearTimeout(startWhenReady);
+    if (timerId) {
+        clearTimeout(timerId);
+        timerId = null;
+    }
     if (fs.existsSync(requiredFile)) {
         logger.info(`${requiredFile} exists, ok to start`);
         require('./app/server/server');
     } else if (attemptCount <= maxAttempts) {
-        logger.info(`watchify is building ${requiredFile} - waiting ${retryDelay / 1000} more seconds`);
-        setTimeout(startWhenReady, retryDelay);
+        logger.info(`${requiredFile} in progress - waiting ${retryDelay / 1000} more seconds`);
+        timerId = setTimeout(startWhenReady, retryDelay);
     } else {
         throw new Error(`requiredFile not found within ${(maxAttempts * retryDelay) / 1000} seconds`);
     }
 }
 
-try {
+if (process.env.APP_DEBUG === 'true') {
+    try {
+        startWhenReady();
+    } catch (e) {
+        logger.error(e);
+    }
+} else {
     startWhenReady();
-} catch (e) {
-    console.log(e);
 }
