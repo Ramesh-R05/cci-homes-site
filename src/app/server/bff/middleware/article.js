1cebd830121993484f37509dc29e0eb7818614bb
Kieran Jones <jones.kieran@hotmail.com>
2018-07-02 14:44:32 +1000

    [BXMA-1303] Added workaround for samsung articles (#466)
    * Added workaround for samsung articles
    
    * Removed console log

--- src/app/server/bff/middleware/article.js
+++ src/app/server/bff/middleware/article.js
@@ -15,6 +15,17 @@ export default async function articleMiddleware(req, res, next) {
             if (navTags) {
                 const relatedArticles = await getLatestTeasers(20, 0, `tags eq '${navTags}'`);
                 res.body.leftHandSide = { items: parseEntities(relatedArticles.data) };
+
+                if (entity.url === '/laundry-solutions-6711' || entity.url === '/how-often-to-wash-clothes-6748') {
+                    let leftHandSideWithoutMiele = [];
+                    const leftHandSideMap = res.body.leftHandSide.items && res.body.leftHandSide.items.map(item => {
+                        if (item.url !== '/win-a-miele-laundry-package-6649') {
+                            leftHandSideWithoutMiele.push(item);
+                        }
+                        return item;
+                    });
+                    res.body.leftHandSide = { items: leftHandSideWithoutMiele };
+                }
             }
         }
 
