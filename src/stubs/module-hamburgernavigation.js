df04384afd174f727703b2e5efcfe11a906f731e
Joe Rattanamaneesilp <jrattanamaneesilp@bauerxcelmedia.com.au>
2018-09-21 15:42:11 +1000

    Fix/automation directories landing page stubbed data (#552)
    * Updated navigation & hamburger stubbed data
    
    * Fixed navigation & hamburger stubbed data
    
    * Updated directories landing page and its article stubbed data
    
    * Added directories listing stubbed data
    
    * Added directories listing stubbed data - Troubleshooting
    
    * Added directories hero stubbed data
    
    * Corrected the file name
    
    * Updated header and hamburger navigation stubbed data
    
    * Added hometheme stubbed data
    
    * Added directoriestheme stubbed data
    
    * Updated module-directorieshero.js

--- src/stubs/module-hamburgernavigation.js
+++ src/stubs/module-hamburgernavigation.js
@@ -1,113 +1,282 @@
 export default {
-    id: 'HOMES-1383',
-    url: '/modules/mobile-navigation-sections',
-    moduleName: 'hamburgernavigation',
-    pageDateCreated: '2015-07-08T00:32:43.00Z',
+    id: "HOMES-7151",
+    url: "/modules/mobile-navigation-sections",
+    moduleName: "hamburgernavigation",
+    pageDateCreated: "2018-08-13T05:32:02.00Z",
     moduleManualContent: {
-        totalCount: 5,
+        totalCount: 16,
         data: [
             {
-                id: 'HOMES-1298',
-                url: '/diy',
+                id: "HOMES-1309",
+                url: "/home-tours",
                 tagsDetails: [
                     {
-                        name: 'food:Homes navigation:DIY',
-                        urlName: 'diy',
-                        fullName: 'food_Homes_navigation_DIY',
-                        displayName: 'DIY'
+                        name: "food:Homes navigation:Home Tours",
+                        urlName: "home-tours",
+                        fullName: "food_Homes_navigation_Home_Tours",
+                        displayName: "Home tours"
                     }
                 ],
-                contentTitle: 'DIY',
-                contentVideo: '',
-                nodeTypeAlias: 'NavigationSection',
+                contentTitle: "Home Tours",
+                contentVideo: "",
+                nodeTypeAlias: "NavigationSection",
                 contentHasVideo: false,
-                pageDateCreated: '2015-06-25T00:31:44.00Z'
+                pageDateCreated: "2015-06-26T01:42:59.00Z"
             },
             {
-                id: 'HOMES-1307',
-                url: '/rooms',
+                id: "HOMES-14137",
+                url: "/renovating",
                 tagsDetails: [
                     {
-                        name: 'food:Homes navigation:Bathroom & Laundry',
-                        urlName: 'bathroom-and-laundry',
-                        fullName: 'food_Homes_navigation_Bathroom_and_Laundry',
-                        displayName: 'Bathroom & Laundry'
-                    },
-                    {
-                        name: 'food:Homes navigation:Bedroom',
-                        urlName: 'bedroom',
-                        fullName: 'food_Homes_navigation_Bedroom',
-                        displayName: 'Bedroom'
-                    },
-                    {
-                        name: 'food:Homes navigation:Living',
-                        urlName: 'living',
-                        fullName: 'food_Homes_navigation_Living',
-                        displayName: 'Living'
-                    },
-                    {
-                        name: 'food:Homes navigation:Kitchen',
-                        urlName: 'kitchen',
-                        fullName: 'food_Homes_navigation_Kitchen',
-                        displayName: 'Kitchen'
+                        name: "food:Homes navigation:Renovating",
+                        urlName: "renovating",
+                        fullName: "food_Homes_navigation_Renovating",
+                        displayName: "Renovating "
                     }
                 ],
-                contentTitle: 'Rooms',
-                contentVideo: '',
-                nodeTypeAlias: 'NavigationSection',
+                contentTitle: "Renovating",
+                contentVideo: "",
+                nodeTypeAlias: "NavigationSection",
                 contentHasVideo: false,
-                pageDateCreated: '2015-06-26T01:42:30.00Z'
+                pageDateCreated: "2018-08-20T02:08:16.00Z"
             },
             {
-                id: 'HOMES-1309',
-                url: '/real-homes',
+                id: "HOMES-14138",
+                url: "/decorating",
                 tagsDetails: [
                     {
-                        name: 'food:Homes navigation:Real Homes',
-                        urlName: 'real-homes',
-                        fullName: 'food_Homes_navigation_Real_Homes',
-                        displayName: 'Real Homes'
+                        name: "food:Homes navigation:Decorating",
+                        urlName: "decorating",
+                        fullName: "food_Homes_navigation_Decorating",
+                        displayName: "Decorating "
                     }
                 ],
-                contentTitle: 'Real Homes',
-                contentVideo: '',
-                nodeTypeAlias: 'NavigationSection',
+                contentTitle: "Decorating",
+                contentVideo: "",
+                nodeTypeAlias: "NavigationSection",
                 contentHasVideo: false,
-                pageDateCreated: '2015-06-26T01:42:59.00Z'
+                pageDateCreated: "2018-08-20T02:09:40.00Z"
             },
             {
-                id: 'HOMES-2683',
-                url: '/my-ideal-house',
+                id: "HOMES-14243",
+                url: "/kitchens",
                 tagsDetails: [
                     {
-                        name: 'food:Homes navigation:My Ideal House',
-                        urlName: 'my-ideal-house',
-                        fullName: 'food_Homes_navigation_My_Ideal_House',
-                        displayName: 'My Ideal House'
+                        name: "food:Homes navigation:Kitchens",
+                        urlName: "kitchens",
+                        fullName: "food_Homes_navigation_Kitchens",
+                        displayName: "Kitchens "
                     }
                 ],
-                contentTitle: 'My Ideal House',
-                contentVideo: '',
-                nodeTypeAlias: 'NavigationSection',
+                contentTitle: "Kitchens",
+                contentVideo: "",
+                nodeTypeAlias: "NavigationSection",
                 contentHasVideo: false,
-                pageDateCreated: '2015-11-24T22:19:55.00Z'
+                pageDateCreated: "2018-08-20T03:14:50.00Z"
             },
             {
-                id: 'HOMES-3223',
-                url: '/christmas',
+                id: "HOMES-4562",
+                url: "/bathroom-and-laundry",
                 tagsDetails: [
                     {
-                        name: 'food:Occasion:Christmas',
-                        urlName: 'christmas',
-                        fullName: 'food_Occasion_Christmas',
-                        displayName: 'Christmas'
+                        name: "food:Homes navigation:Bathroom & Laundry",
+                        urlName: "bathroom-and-laundry",
+                        fullName: "food_Homes_navigation_Bathroom_and_Laundry",
+                        displayName: "Bathroom & Laundry"
                     }
                 ],
-                contentTitle: 'Christmas',
-                contentVideo: '',
-                nodeTypeAlias: 'NavigationSection',
+                contentTitle: "Bathroom & Laundry",
+                contentVideo: "",
+                nodeTypeAlias: "NavigationSection",
                 contentHasVideo: false,
-                pageDateCreated: '2016-12-20T23:59:40.00Z'
+                pageDateCreated: "2017-01-10T02:36:43.00Z"
+            },
+            {
+                id: "HOMES-14238",
+                url: "/living-and-dining",
+                tagsDetails: [
+                    {
+                        name: "food:Homes navigation:Living & Dining",
+                        urlName: "living-and-dining",
+                        fullName: "food_Homes_navigation_Living_Dining",
+                        displayName: "Living & Dining"
+                    }
+                ],
+                contentTitle: "Living & Dining",
+                contentVideo: "",
+                nodeTypeAlias: "NavigationSection",
+                contentHasVideo: false,
+                pageDateCreated: "2018-08-20T03:14:18.00Z"
+            },
+            {
+                id: "HOMES-14232",
+                url: "/bedrooms",
+                tagsDetails: [
+                    {
+                        name: "food:Homes navigation:Bedrooms",
+                        urlName: "bedrooms",
+                        fullName: "food_Homes_navigation_Bedrooms",
+                        displayName: "Bedrooms "
+                    }
+                ],
+                contentTitle: "Bedrooms",
+                contentVideo: "",
+                nodeTypeAlias: "NavigationSection",
+                contentHasVideo: false,
+                pageDateCreated: "2018-08-20T03:13:52.00Z"
+            },
+            {
+                id: "HOMES-14142",
+                url: "/kids-rooms",
+                tagsDetails: [
+                    {
+                        name: "food:Homes navigation:Kids Rooms",
+                        urlName: "kids-rooms",
+                        fullName: "food_Homes_navigation_Kids_Rooms",
+                        displayName: "Kids' Rooms"
+                    }
+                ],
+                contentTitle: "Kids' Rooms",
+                contentVideo: "",
+                nodeTypeAlias: "NavigationSection",
+                contentHasVideo: false,
+                pageDateCreated: "2018-08-20T02:12:04.00Z"
+            },
+            {
+                id: "HOMES-14225",
+                url: "/outdoors-and-gardening",
+                tagsDetails: [
+                    {
+                        name: "food:Homes navigation:Outdoors & Gardening",
+                        urlName: "outdoors-and-gardening",
+                        fullName: "food_Homes_navigation_Outdoors_Gardening",
+                        displayName: "Outdoors & Gardening"
+                    }
+                ],
+                contentTitle: "Outdoors & Gardening",
+                contentVideo: "",
+                nodeTypeAlias: "NavigationSection",
+                contentHasVideo: false,
+                pageDateCreated: "2018-08-20T03:13:10.00Z"
+            },
+            {
+                id: "HOMES-14220",
+                url: "/diy-and-craft",
+                tagsDetails: [
+                    {
+                        name: "food:Homes navigation:DIY & Craft",
+                        urlName: "diy-and-craft",
+                        fullName: "food_Homes_navigation_DIY_Craft",
+                        displayName: "DIY & Craft"
+                    }
+                ],
+                contentTitle: "DIY & Craft",
+                contentVideo: "",
+                nodeTypeAlias: "NavigationSection",
+                contentHasVideo: false,
+                pageDateCreated: "2018-08-20T03:12:41.00Z"
+            },
+            {
+                id: "HOMES-14139",
+                url: "/entertaining",
+                tagsDetails: [
+                    {
+                        name: "food:Homes navigation:Entertaining",
+                        urlName: "entertaining",
+                        fullName: "food_Homes_navigation_Entertaining",
+                        displayName: "Entertaining "
+                    }
+                ],
+                contentTitle: "Entertaining",
+                contentVideo: "",
+                nodeTypeAlias: "NavigationSection",
+                contentHasVideo: false,
+                pageDateCreated: "2018-08-20T02:10:44.00Z"
+            },
+            {
+                id: "HOMES-14141",
+                url: "/lifestyle",
+                tagsDetails: [
+                    {
+                        name: "food:Homes navigation:Lifestyle",
+                        urlName: "lifestyle",
+                        fullName: "food_Homes_navigation_Lifestyle",
+                        displayName: "Lifestyle "
+                    }
+                ],
+                contentTitle: "Lifestyle",
+                contentVideo: "",
+                nodeTypeAlias: "NavigationSection",
+                contentHasVideo: false,
+                pageDateCreated: "2018-08-20T02:11:25.00Z"
+            },
+            {
+                id: "HOMES-2683",
+                url: "/my-ideal-house",
+                tagsDetails: [
+                    {
+                        name: "food:Homes navigation:My Ideal House",
+                        urlName: "my-ideal-house",
+                        fullName: "food_Homes_navigation_My_Ideal_House",
+                        displayName: "My Ideal House"
+                    }
+                ],
+                contentTitle: "My Ideal House",
+                contentVideo: "",
+                nodeTypeAlias: "NavigationSection",
+                contentHasVideo: false,
+                pageDateCreated: "2015-11-24T22:19:55.00Z"
+            },
+            {
+                id: "HOMES-4579",
+                url: "/competition",
+                tagsDetails: [
+                    {
+                        name: "common:topic:Competition",
+                        urlName: "competition",
+                        fullName: "common_topic_Competition",
+                        displayName: "Competition"
+                    }
+                ],
+                contentTitle: "Win",
+                contentVideo: "",
+                nodeTypeAlias: "NavigationSection",
+                contentHasVideo: false,
+                pageDateCreated: "2017-01-12T00:47:51.00Z"
+            },
+            {
+                id: "HOMES-6891",
+                url: "/the-block",
+                tagsDetails: [
+                    {
+                        name: "tv:tv_program:The Block",
+                        urlName: "the-block",
+                        fullName: "tv_tv_program_The_Block",
+                        displayName: "The Block"
+                    }
+                ],
+                contentTitle: "The Block",
+                contentVideo: "",
+                nodeTypeAlias: "NavigationSection",
+                contentHasVideo: false,
+                pageDateCreated: "2018-06-29T08:10:02.00Z"
+            },
+            {
+                id: "HOMES-14145",
+                url: "/directories",
+                tagsDetails: [
+                    {
+                        name: "food:Homes navigation:Directories",
+                        urlName: "directories",
+                        fullName: "food_Homes_navigation_Directories",
+                        displayName: "Directories"
+                    }
+                ],
+                contentTitle: "Directories",
+                contentVideo: "",
+                nodeTypeAlias: "NavigationSection",
+                contentHasVideo: false,
+                pageDateCreated: "2018-08-20T02:14:28.00Z"
             }
         ]
     }
--- src/stubs/module-headernavigation.js
+++ src/stubs/module-headernavigation.js
@@ -1,113 +1,159 @@
 export default {
-    id: 'HOMES-1383',
-    url: '/modules/navigation-sections',
-    moduleName: 'headernavigation',
-    pageDateCreated: '2015-07-08T00:32:43.00Z',
+    id: "HOMES-1383",
+    url: "/modules/navigation-sections",
+    moduleName: "headernavigation",
+    pageDateCreated: "2015-07-08T00:32:43.00Z",
     moduleManualContent: {
-        totalCount: 5,
+        totalCount: 7,
         data: [
             {
-                id: 'HOMES-1298',
-                url: '/diy',
+                id: "HOMES-1309",
+                url: "/home-tours",
                 tagsDetails: [
                     {
-                        name: 'food:Homes navigation:DIY',
-                        urlName: 'diy',
-                        fullName: 'food_Homes_navigation_DIY',
-                        displayName: 'DIY'
+                        name: "food:Homes navigation:Home Tours",
+                        urlName: "home-tours",
+                        fullName: "food_Homes_navigation_Home_Tours",
+                        displayName: "Home tours"
                     }
                 ],
-                contentTitle: 'DIY',
-                contentVideo: '',
-                nodeTypeAlias: 'NavigationSection',
+                contentTitle: "Home Tours",
+                contentVideo: "",
+                nodeTypeAlias: "NavigationSection",
                 contentHasVideo: false,
-                pageDateCreated: '2015-06-25T00:31:44.00Z'
+                pageDateCreated: "2015-06-26T01:42:59.00Z"
             },
             {
-                id: 'HOMES-1307',
-                url: '/rooms',
+                id: "HOMES-14137",
+                url: "/renovating",
                 tagsDetails: [
                     {
-                        name: 'food:Homes navigation:Bathroom & Laundry',
-                        urlName: 'bathroom-and-laundry',
-                        fullName: 'food_Homes_navigation_Bathroom_and_Laundry',
-                        displayName: 'Bathroom & Laundry'
+                        name: "food:Homes navigation:Renovating",
+                        urlName: "renovating",
+                        fullName: "food_Homes_navigation_Renovating",
+                        displayName: "Renovating "
+                    }
+                ],
+                contentTitle: "Renovating",
+                contentVideo: "",
+                nodeTypeAlias: "NavigationSection",
+                contentHasVideo: false,
+                pageDateCreated: "2018-08-20T02:08:16.00Z"
+            },
+            {
+                id: "HOMES-14138",
+                url: "/decorating",
+                tagsDetails: [
+                    {
+                        name: "food:Homes navigation:Decorating",
+                        urlName: "decorating",
+                        fullName: "food_Homes_navigation_Decorating",
+                        displayName: "Decorating "
+                    }
+                ],
+                contentTitle: "Decorating",
+                contentVideo: "",
+                nodeTypeAlias: "NavigationSection",
+                contentHasVideo: false,
+                pageDateCreated: "2018-08-20T02:09:40.00Z"
+            },
+            {
+                id: "HOMES-4526",
+                url: "/rooms",
+                tagsDetails: [
+                    {
+                        name: "food:Homes navigation:Kitchens",
+                        urlName: "kitchens",
+                        fullName: "food_Homes_navigation_Kitchens",
+                        displayName: "Kitchens "
                     },
                     {
-                        name: 'food:Homes navigation:Bedroom',
-                        urlName: 'bedroom',
-                        fullName: 'food_Homes_navigation_Bedroom',
-                        displayName: 'Bedroom'
+                        name: "food:Homes navigation:Bathroom & Laundry",
+                        urlName: "bathroom-and-laundry",
+                        fullName: "food_Homes_navigation_Bathroom_and_Laundry",
+                        displayName: "Bathroom & Laundry"
                     },
                     {
-                        name: 'food:Homes navigation:Living',
-                        urlName: 'living',
-                        fullName: 'food_Homes_navigation_Living',
-                        displayName: 'Living'
+                        name: "food:Homes navigation:Living & Dining",
+                        urlName: "living-and-dining",
+                        fullName: "food_Homes_navigation_Living_Dining",
+                        displayName: "Living & Dining"
                     },
                     {
-                        name: 'food:Homes navigation:Kitchen',
-                        urlName: 'kitchen',
-                        fullName: 'food_Homes_navigation_Kitchen',
-                        displayName: 'Kitchen'
+                        name: "food:Homes navigation:Bedrooms",
+                        urlName: "bedrooms",
+                        fullName: "food_Homes_navigation_Bedrooms",
+                        displayName: "Bedrooms "
+                    },
+                    {
+                        name: "food:Homes navigation:Kids Rooms",
+                        urlName: "kids-rooms",
+                        fullName: "food_Homes_navigation_Kids_Rooms",
+                        displayName: "Kids' Rooms"
+                    },
+                    {
+                        name: "food:Homes navigation:Outdoors & Gardening",
+                        urlName: "outdoors-and-gardening",
+                        fullName: "food_Homes_navigation_Outdoors_Gardening",
+                        displayName: "Outdoors & Gardening"
                     }
                 ],
-                contentTitle: 'Rooms',
-                contentVideo: '',
-                nodeTypeAlias: 'NavigationSection',
+                contentTitle: "Rooms",
+                contentVideo: "",
+                nodeTypeAlias: "NavigationSection",
                 contentHasVideo: false,
-                pageDateCreated: '2015-06-26T01:42:30.00Z'
+                pageDateCreated: "2016-12-14T03:40:23.00Z"
             },
             {
-                id: 'HOMES-1309',
-                url: '/real-homes',
+                id: "HOMES-2683",
+                url: "/my-ideal-house",
                 tagsDetails: [
                     {
-                        name: 'food:Homes navigation:Real Homes',
-                        urlName: 'real-homes',
-                        fullName: 'food_Homes_navigation_Real_Homes',
-                        displayName: 'Real Homes'
+                        name: "food:Homes navigation:My Ideal House",
+                        urlName: "my-ideal-house",
+                        fullName: "food_Homes_navigation_My_Ideal_House",
+                        displayName: "My Ideal House"
                     }
                 ],
-                contentTitle: 'Real Homes',
-                contentVideo: '',
-                nodeTypeAlias: 'NavigationSection',
+                contentTitle: "My Ideal House",
+                contentVideo: "",
+                nodeTypeAlias: "NavigationSection",
                 contentHasVideo: false,
-                pageDateCreated: '2015-06-26T01:42:59.00Z'
+                pageDateCreated: "2015-11-24T22:19:55.00Z"
             },
             {
-                id: 'HOMES-2683',
-                url: '/my-ideal-house',
+                id: "HOMES-4579",
+                url: "/competition",
                 tagsDetails: [
                     {
-                        name: 'food:Homes navigation:My Ideal House',
-                        urlName: 'my-ideal-house',
-                        fullName: 'food_Homes_navigation_My_Ideal_House',
-                        displayName: 'My Ideal House'
+                        name: "common:topic:Competition",
+                        urlName: "competition",
+                        fullName: "common_topic_Competition",
+                        displayName: "Competition"
                     }
                 ],
-                contentTitle: 'My Ideal House',
-                contentVideo: '',
-                nodeTypeAlias: 'NavigationSection',
+                contentTitle: "Win",
+                contentVideo: "",
+                nodeTypeAlias: "NavigationSection",
                 contentHasVideo: false,
-                pageDateCreated: '2015-11-24T22:19:55.00Z'
+                pageDateCreated: "2017-01-12T00:47:51.00Z"
             },
             {
-                id: 'HOMES-3223',
-                url: '/christmas',
+                id: "HOMES-6891",
+                url: "/the-block",
                 tagsDetails: [
                     {
-                        name: 'food:Occasion:Christmas',
-                        urlName: 'christmas',
-                        fullName: 'food_Occasion_Christmas',
-                        displayName: 'Christmas'
+                        name: "tv:tv_program:The Block",
+                        urlName: "the-block",
+                        fullName: "tv_tv_program_The_Block",
+                        displayName: "The Block"
                     }
                 ],
-                contentTitle: 'Christmas',
-                contentVideo: '',
-                nodeTypeAlias: 'NavigationSection',
+                contentTitle: "The Block",
+                contentVideo: "",
+                nodeTypeAlias: "NavigationSection",
                 contentHasVideo: false,
-                pageDateCreated: '2016-12-20T23:59:40.00Z'
+                pageDateCreated: "2018-06-29T08:10:02.00Z"
             }
         ]
     }
