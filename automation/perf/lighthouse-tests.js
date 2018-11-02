85100cae39ffe68e558a3b0e6ad4c6a6d7ed050e
Joe Rattanamaneesilp <jrattanamaneesilp@bauerxcelmedia.com.au>
2018-11-02 17:00:20 +1100

    Updated expected performance tes scores (#587)

--- automation/perf/lighthouse-tests.js
+++ automation/perf/lighthouse-tests.js
@@ -4,27 +4,27 @@ const testLinks = [
     {
         title: 'HTL-homepage',
         url: 'http://homes-site.test.bxm.net.au/',
-        expectedScore: 0.12
+        expectedScore: 0.23
     },
     {
         title: 'HTL-section',
         url: 'http://homes-site.test.bxm.net.au/real-homes/',
-        expectedScore: 0.14
+        expectedScore: 0.19
     },
     {
         title: 'HTL-tags landing page',
         url: 'http://homes-site.test.bxm.net.au/tags/luxury-home/',
-        expectedScore: 0.14
+        expectedScore: 0.18
     },
     {
         title: 'HTL-article',
         url: 'http://homes-site.test.bxm.net.au/automation-test-article-with-hero-image-3193/',
-        expectedScore: 0.11
+        expectedScore: 0.17
     },
     {
         title: 'HTL-gallery',
         url: 'http://homes-site.test.bxm.net.au/automation-test-gallery-3201/',
-        expectedScore: 0.11
+        expectedScore: 0.15
     }
 ];
 
