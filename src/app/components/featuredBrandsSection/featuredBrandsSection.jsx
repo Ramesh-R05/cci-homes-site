4b592549ea1707e3e62089aea0ccd0c8a9023bf0
Raymond <raymondrotterdam@yahoo.co.uk>
2018-12-03 12:35:01 +1100

    BXMA-1667: Fixed if the featurebrand module is unpublished bud and renamed the test files for consistency (#615)

--- src/app/components/featuredBrandsSection/featuredBrandsSection.jsx
+++ src/app/components/featuredBrandsSection/featuredBrandsSection.jsx
@@ -8,14 +8,19 @@ export default class FeaturedBrandsSection extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectedBrand: props.featuredBrands && !!props.featuredBrands.items ? this.props.featuredBrands.items[0].id : null
+            selectedBrand:
+                props.featuredBrands && Array.isArray(props.featuredBrands.items) && props.featuredBrands.items.length
+                    ? this.props.featuredBrands.items[0].id
+                    : null
         };
 
         this.displayLatestItems = this.displayLatestItems.bind(this);
     }
 
     static propTypes = {
-        featuredBrands: PropTypes.object.isRequired,
+        featuredBrands: PropTypes.shape({
+            items: PropTypes.array
+        }).isRequired,
         latestBrandItems: PropTypes.object.isRequired
     };
 
@@ -31,21 +36,25 @@ export default class FeaturedBrandsSection extends Component {
     render() {
         const { featuredBrands, latestBrandItems } = this.props;
         const { selectedBrand } = this.state;
-        return (
-            <div className="latest-content">
-                <section>
-                    <h1 className="latest-content__title-container">THE LATEST FROM YOUR FAVOURITE BRANDS</h1>
-                    <ul className="latest-content__list">
-                        {selectedBrand &&
-                            latestBrandItems[selectedBrand].map(item => (
+        let featuredBrandsSection = null;
+        if (selectedBrand) {
+            featuredBrandsSection = (
+                <div className="latest-content">
+                    <section>
+                        <h1 className="latest-content__title-container">THE LATEST FROM YOUR FAVOURITE BRANDS</h1>
+                        <ul className="latest-content__list">
+                            {latestBrandItems[selectedBrand].map(item => (
                                 <li className="latest-content__item">
                                     <Teaser {...item} key={item.id} sizes="brand-list" modifier="img-top" />
                                 </li>
                             ))}
-                    </ul>
-                </section>
-                <BrandSwitcher brands={featuredBrands.items} linkType="button" onClickHandler={(e, brand) => this.displayLatestItems(e, brand)} />{' '}
-            </div>
-        );
+                        </ul>
+                    </section>
+                    <BrandSwitcher brands={featuredBrands.items} linkType="button" onClickHandler={(e, brand) => this.displayLatestItems(e, brand)} />{' '}
+                </div>
+            );
+        }
+
+        return featuredBrandsSection;
     }
 }
