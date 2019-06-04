946081b1fe9f7c5ff87dc572b96e78bd2ad474e8
Nick Prodromou <nick.prodromou1@gmail.com>
2019-06-04 16:26:28 +1000

    double timeout (#743)

--- automation/features/support/hooks.js
+++ automation/features/support/hooks.js
@@ -1,7 +1,7 @@
 var hooks = function () {
 
-    this.setDefaultTimeout(120 * 1000);
-    console.log("Update timeout to 120000");
+    this.setDefaultTimeout(120 * 2000);
+    console.log("Update timeout to `${120 * 2000}`");
 
     this.After(function (scenario) {
         browser.deleteCookie();
