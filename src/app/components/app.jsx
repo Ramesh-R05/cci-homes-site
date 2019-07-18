5cbfe4b3018546ad1dad2b31c3e265f23fe39a08
Raymond Wijaya <rwijaya@bauer-media.com.au>
2019-07-18 13:26:31 +1000

    front end logger

--- src/app/components/app.jsx
+++ src/app/components/app.jsx
@@ -8,14 +8,38 @@ import { handleHistory } from 'fluxible-router';
 import AdManager from '@bxm/ad/lib/google/components/adManager';
 import GoogleFont from './html/googleFont';
 import Error from './pages/error';
+import frontEndLogger from '../../frontEndLogger';
 
 class ErrorBoundary extends Component {
+    constructor(props) {
+
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error) {
+        frontEndLogger.push({ 'text': 'app.js/HTL - an error has been thrown by derivedStateFromError and the fallback UI is getting rendered',
+                              'error': error});
+
+        return { hasError: true };
+    }
+
     componentDidCatch(error, errorInfo) {
         // TODO - Do something useful with errors
+        frontEndLogger.push({ 'text': 'app.js/HTL - an error has been thrown by componentDidCatch and the fallback UI is getting rendered',
+                              'error': error,
+                              'errorInfo': errorInfo});
+
+        this.setState({ hasError: true });
     }
 
     render() {
         const { children } = this.props;
+
+        if (this.state.hasError) {
+            // Render fallback UI
+            return <h1>Something went wrong.</h1>;
+        }
 
         return children;
     }
