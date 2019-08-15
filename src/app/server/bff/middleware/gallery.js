ea621ef23bc355fddd5b46ef2ca1ca07460ceb9f
Kieran Jones <jones.kieran@hotmail.com>
2019-08-15 12:49:23 +1000

    Added same tag change that was done for article middleware (#781)

--- src/app/server/bff/middleware/gallery.js
+++ src/app/server/bff/middleware/gallery.js
@@ -15,10 +15,29 @@ export default async function galleryMiddleware(req, res, next) {
         res.body.moreGalleries = parseEntities(galleryItems.data);
 
         if (entity.tags) {
-            const navTags = entity.tags.find(tag => tag.includes('navigation'));
+            const navTag = entity.tags.find(tag => tag.includes('navigation'));
 
-            if (navTags) {
-                const relatedArticles = await API.getLatestTeasers(20, 0, `tags eq %27${navTags}%27`);
+            if (navTag) {
+                // TODO: temporary solution to fix the issue with tags coming through as full tag name
+                // this will be fixed when the site uses tagsDetails.urlName for tag urls
+                const splitTag = navTag.split(':');
+                const tagName = Array.isArray(splitTag) && splitTag.length === 3 && splitTag[2];
+
+                let formattedTag = '';
+
+                if (tagName) {
+                    formattedTag = decodeURIComponent(tagName.toLowerCase());
+                    formattedTag = formattedTag
+                        .replace(/\s*-\s*/g, '-')
+                        .replace(/\s*\/\s*/g, '-')
+                        .replace(/"|'|,|\$/g, '')
+                        .replace(/(\d+)\s*\+.*/g, 'more-than-$1')
+                        .replace(/<\s*\$?/g, 'less-than-')
+                        .replace(/\s*&\s*/g, '-and-')
+                        .replace(/\s+/g, '-');
+                }
+
+                const relatedArticles = await API.getLatestTeasers(20, 0, `tagsDetails/urlName eq %27${formattedTag}%27`);
                 res.body.leftHandSide = { items: parseEntities(relatedArticles.data) };
             }
         }
